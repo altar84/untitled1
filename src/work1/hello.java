15a6f02abdc104e4f4535e56662d221cc8c8e72a
Kutlu-Saka <avcialtar@hotmail.com>
2022-12-15 15:22:36 +0300

    added more work

--- src/work1/hello.java
+++ src/work1/hello.java
@@ -5,5 +5,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("work1 started");
         System.out.println( "Created git" );
+        System.out.println("Made few commits");
     }
 }
